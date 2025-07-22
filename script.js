649f11f8ac5d2b28c498896be51b3544ad95f245
Lawalkamal <lawalkamal595@gmail.com>
2025-07-22 16:58:59 +0100

    Added responsiveness, and button funtionality

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const showBtn = document.querySelector("#show-details");
 const moreDetails = document.querySelector(".full");
 
-showBtn.addEventListener("click", function () {
-  showBtn.addEventListener("click", () => {
+showBtn.addEventListener('click', function () {
     if (moreDetails.classList.contains("hidden")) {
       moreDetails.classList.remove("hidden");
       showBtn.textContent = "Show less detail";
     } else {
       moreDetails.classList.add("hidden");
-      showBtn.textContent = 'Show more details';
+      showBtn.textContent = "Show more details";
     }
-  });
 });
