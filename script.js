481e0820da69efd4352597a8e3bdd92116b72bcf
Lawalkamal <lawalkamal595@gmail.com>
2025-07-22 12:25:02 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const detailsBtn = document.querySelector("#show-details");
+const infoTab = document.querySelector(".full");
+const showMore = function () {
+  infoTab.classList.add("hidden");
+};
+const showLess = function () {
+  infoTab.classList.remove("hidden");
+};
